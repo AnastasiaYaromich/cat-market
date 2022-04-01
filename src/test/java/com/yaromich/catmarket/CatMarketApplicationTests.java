2f08da8e082d535792046de57733b219e82b8751
AnastasiaYaromich <Anastasia281295@gmail.com>
2022-04-01 20:46:15 +0300

    Start project

---
+++ src/test/java/com/yaromich/catmarket/CatMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaromich.catmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
