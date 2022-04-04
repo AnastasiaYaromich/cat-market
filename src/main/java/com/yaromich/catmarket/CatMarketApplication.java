b6258e4666e0967d9ac4de2a439dd607f165a0c4
AnastasiaYaromich <Anastasia281295@gmail.com>
2022-04-04 15:46:19 +0300

    Add simple cart

--- src/main/java/com/yaromich/catmarket/CatMarketApplication.java
+++ src/main/java/com/yaromich/catmarket/CatMarketApplication.java
@@ -5,13 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatMarketApplication {
-	// Домашнее задание:
-	// 1. Разобраться с кодом. +
-	// 2. Создать бин корзина с листом продуктов. ?
-	// 3. Добавить на фронте к каждому продукту кнопку добавить в корзину. +
-	// 4. Кнопка добавить в корзину должна складывать в бин корзина указанный продукт
-	// (продукт может быть добален несколько раз, группировать продукты в корзине пока не надо). +
-	// 5. Под таблице с продуктами отрисуйте таблицу корзина. +
 	public static void main(String[] args) {
 		SpringApplication.run(CatMarketApplication.class, args);
 	}
